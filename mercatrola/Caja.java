e1d70d7fdf61683638eebfc435e7c4f0449f223f
sermadita7777 <theryux7@gmail.com>
2025-11-16 23:27:00 +0100

    Fase 3: Implementación unicola

--- mercatrola/Caja.java
+++ mercatrola/Caja.java
@@ -1,42 +1,50 @@
 package mercatrola;
 
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
-public class Caja implements Runnable{
+public class Caja implements Runnable {
 	private final BlockingQueue<Cliente> cola;
+	private final AtomicInteger clientesRestantes;
 
-	public Caja(BlockingQueue<Cliente> cola) {
+	public Caja(BlockingQueue<Cliente> cola, AtomicInteger clientesRestantes) {
 		super();
 		this.cola = cola;
+		this.clientesRestantes = clientesRestantes;
 	}
 
 	@Override
 	public void run() {
-		System.out.printf("%s preparada.%n",Thread.currentThread().getName());
-		
-		while(!this.cola.isEmpty()) {
-			try {
-				
-				Cliente c=this.cola.take();
-				
-				long tStartService=System.currentTimeMillis();
-				c.settServiceStart(tStartService);
-				
-				System.out.printf("Cliente %s atendido en %s.%n",c.getId(),Thread.currentThread().getName());
-				Thread.sleep((int)(Math.random()*1000));
-				
-				long tEndService=System.currentTimeMillis();
-				System.out.printf("Cliente %s ha terminado en %s. Tiempo de espera: %d ms.%n",
-						c.getId(),Thread.currentThread().getName(),c.gettWait());
-				
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		
-		
-	}
+		System.out.printf("%s preparada.%n", Thread.currentThread().getName());
 
-	
+		try {
+			while (true) {
+				Cliente c = this.cola.poll(500,TimeUnit.MILLISECONDS);
+				
+				if(c!=null) {
+					long tStartService = System.currentTimeMillis();
+					c.settServiceStart(tStartService);
+
+					System.out.printf("Cliente %s atendido en %s.%n", c.getId(), Thread.currentThread().getName());
+					Thread.sleep((int) (Math.random() * 1000));
+
+					System.out.printf("Cliente %s ha terminado en %s. Tiempo de espera: %d ms.%n", c.getId(),
+							Thread.currentThread().getName(), c.gettWait());
+
+					this.clientesRestantes.decrementAndGet();
+				} else {
+					if(this.clientesRestantes.get() == 0) {
+						System.out.printf("%s no tiene más clientes y cierra.%n", Thread.currentThread().getName());
+						break;
+					}
+				}
+
+			}
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
 }
--- mercatrola/Mercado.java
+++ mercatrola/Mercado.java
@@ -1,20 +1,28 @@
 package mercatrola;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Mercado {
-	public static void simulate(int nClientes,int nCajas) {
-		System.out.println("Mercatrola abre sus puertas.");
-		
+	public static void simulateMulticola(int nClientes,int nCajas) {
 		BlockingQueue<Cliente> [] colas=new LinkedBlockingQueue[nCajas];
 		
 		for(int i=0;i<nCajas;i++) {
 			colas[i]=new LinkedBlockingQueue<>();
 		}
 		
+		AtomicInteger clientesRestantes=new AtomicInteger(nClientes);
+		Thread[] cajas=new Thread[nCajas];
+		for(int i=0;i<nCajas;i++) {
+			cajas[i]=new Thread(new Caja(colas[i],clientesRestantes),"Caja "+i);
+			cajas[i].start();
+		}
 		
 		List<Cliente> listaClientes =new ArrayList<>();
 		
@@ -29,12 +37,68 @@ public class Mercado {
 			clientes[i].start();
 		}
 		
+		for (Thread thread : cajas) {
+			try {
+				thread.join();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		for (Thread thread : clientes) {
+			try {
+				thread.join();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		System.out.println("\nMercatrola cierra sus puertas.\n");
+		
+		getStats(listaClientes);
+	}
+	
+	private static void getStats(List<Cliente> listaClientes) {
+		List<Long> tiemposEspera=new ArrayList<>();
+		for(Cliente c: listaClientes) {
+			tiemposEspera.add(c.gettWait());
+		}
+		
+		long max=tiemposEspera.stream().mapToLong(v -> v).max().orElse(0);
+		long min=tiemposEspera.stream().mapToLong(v -> v).min().orElse(0);
+		double avg=tiemposEspera.stream().mapToLong(v -> v).average().orElse(0);
+		
+		System.out.println("RESULTADOS");
+		System.out.println("Tiempo de espera máximo: "+max+" ms.");
+		System.out.println("Tiempo de espera mínimo: "+min+" ms.");
+		System.out.println("Tiempo de espera medio: "+avg+" ms.");
+		
+	}
+	
+	public static void simulateUnicola(int nClientes,int nCajas) {
+		System.out.println("UNICOLA");
+		BlockingQueue<Cliente> cola  = new LinkedBlockingQueue<>();
+		
+		AtomicInteger clientesRestantes=new AtomicInteger(nClientes);
 		Thread[] cajas=new Thread[nCajas];
 		for(int i=0;i<nCajas;i++) {
-			cajas[i]=new Thread(new Caja(colas[i]),"Caja "+i);
+			cajas[i]=new Thread(new Caja(cola,clientesRestantes),"Caja "+i);
 			cajas[i].start();
 		}
 		
+		List<Cliente> listaClientes =new ArrayList<>();
+		
+		Thread[] clientes=new Thread[nClientes];
+		for(int i=0;i<nClientes;i++) {
+			
+			Cliente c=new Cliente(i,cola);
+			listaClientes.add(c);
+			
+			clientes[i]=new Thread(c,"Cliente "+i);
+			clientes[i].start();
+		}
 		
 		for (Thread thread : clientes) {
 			try {
@@ -54,25 +118,15 @@ public class Mercado {
 			}
 		}
 		
-		System.out.println("Mercatrola cierra sus puertas.");
+		System.out.println("\nMercatrola cierra sus puertas.\n");
 		
-		List<Long> tiemposEspera=new ArrayList<>();
-		for(Cliente c: listaClientes) {
-			tiemposEspera.add(c.gettWait());
-		}
-		
-		long max=tiemposEspera.stream().mapToLong(v -> v).max().orElse(0);
-		long min=tiemposEspera.stream().mapToLong(v -> v).min().orElse(0);
-		double avg=tiemposEspera.stream().mapToLong(v -> v).average().orElse(0);
-		
-		System.out.println("RESULTADOS");
-		System.out.println("Tiempo de espera máximo: "+max+" ms.");
-		System.out.println("Tiempo de espera mínimo: "+min+" ms.");
-		System.out.println("Tiempo de espera medio: "+avg+" ms.");
-		
+		getStats(listaClientes);
 	}
 	
 	public static void main(String[] args) {
-		simulate(5,2);
+		System.out.println("Mercatrola abre sus puertas.\n");
+		simulateMulticola(500,10);
+		simulateUnicola(500,10);
+		
 	}
 }
